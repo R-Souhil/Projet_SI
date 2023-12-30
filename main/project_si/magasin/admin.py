d050cd37eb95a5895a87d578600699148cf24985
Labadifares <155245038+Labadifares@users.noreply.github.com>
2023-12-30 23:38:07 +0100

    Update admin.py

--- main/project_si/magasin/admin.py
+++ main/project_si/magasin/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Magasin, Fournisseur, Achat, Transfert
+from .models import Magasin, Fournisseur, Achat, Transfert, PaiementFournisseur, AnalyseDesVentes, AnalyseDesAchats
 
 admin.site.register(Magasin)
 admin.site.register(Fournisseur)
 admin.site.register(Achat)
 admin.site.register(Transfert)
+admin.site.register(PaiementFournisseur)
+admin.site.register(AnalyseDesVentes)
+admin.site.register(AnalyseDesAchats)
