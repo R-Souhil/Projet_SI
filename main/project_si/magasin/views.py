a9b8ee9d240dc8114bd4e42c4846e65be415b9d9
Labadifares <155245038+Labadifares@users.noreply.github.com>
2023-12-31 23:58:05 +0100

    Update views.py

--- main/project_si/magasin/views.py
+++ main/project_si/magasin/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Fournisseur
+
+
 
 # Create your views here.
+
+
+def ajouter_fournisseur(request):
+    if request.method == 'POST':
+        # Récupérer les données du formulaire
+        nom_fournisseur = request.POST.get('nom_fournisseur')
+        # Ajouter le fournisseur à la base de données
+        fournisseur = Fournisseur.objects.create(nom_fournisseur=nom_fournisseur)
+        # Rediriger ou afficher un message de succès
+    else:
+        # Afficher le formulaire pour ajouter un fournisseur
+        return render(request, 'ajouter_fournisseur.html')
+
+# Note: Vous devrez créer un template HTML (ajouter_fournisseur.html) pour afficher le formulaire.on le fais dans un seul fichier 
+
